a1e0ec58ababec9e3e46746fda0b3d9a85d8f31d
Dayaa <dayisus.1337@gmail.com>
2019-07-20 20:41:39 -0500

    Añadida una cuenta de Dracukeos

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import ReactAudioPlayer from 'react-audio-player';
 
 class App extends Component {
   state = {
-    dracukeo: false
+    dracukeo: false,
+    dracukeos: parseInt(localStorage.getItem("dracukeos")) || 0
+
   };
+
+  addDracukeos = () => {
+    localStorage.setItem("dracukeos", this.state.dracukeos + 1)
+    this.setState({
+      dracukeos: this.state.dracukeos + 1
+    })
+  }
+  
 
   render() {
     return (
       <div className="center">
         {
-        this.state.dracukeo ? <div><img src="/keo.jpg" alt="Kidd Keo"/><ReactAudioPlayer src="/dracukeo.mp3" autoPlay onEnded={() => {this.setState({dracukeo: false})}}/></div> :         <p onClick={() => {this.setState({dracukeo: true})}}>Dracukeo</p>
+        this.state.dracukeo ? <div><img src="/keo.jpg" alt="Kidd Keo"/><ReactAudioPlayer src="/dracukeo.mp3" autoPlay onEnded={() => {this.setState({dracukeo: false}); this.addDracukeos()}}/></div> :         <p onClick={() => {this.setState({dracukeo: true})}}>Dracukeo</p>
 
         }
+        <br/>
+        Tienes {this.state.dracukeos} dracukeos.
         
       </div>
     );
